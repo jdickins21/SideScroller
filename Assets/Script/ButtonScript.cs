5276a6c30b13528bbebffe28f45c2e34869fe092
Jacob <jdickins@uoregon.edu>
2016-05-16 14:00:45 -0700

    starting shop

---
+++ Assets/Script/ButtonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public bool active;
+	public GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+	}
+
+	public void buttionAction(){
+
+	}
+}
