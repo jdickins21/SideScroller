f03033cb05fcd564eea875454a78576c679c5aee
Jacob <jdickins@uoregon.edu>
2016-05-16 20:09:35 -0700

    store started
    guns don’t work till a bool is set to true. set to true in shop.
    
    TODO:
    .finish shop scene
    .get shop in menu between levels and after death
    .get more items
    .make buttons in store remove money

--- Assets/Script/ButtonScript.cs
+++ Assets/Script/ButtonScript.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ButtonScript : MonoBehaviour {
+public class ButtonScript : MonoBehaviour  {
 
-	public bool active;
+	public static bool pistolUnlocked = true;
+	public static bool rifleUnlocked = false;
+	public static bool shotgunUnlocked = false;
+	public string gunName;
 	public GameObject player;
 
 	void Awake()
@@ -12,7 +15,28 @@ public class ButtonScript : MonoBehaviour {
 
 	}
 
-	public void buttionAction(){
+	public bool getUnlocked(string gunName){
+		if (gunName == "pistol") {
+			return pistolUnlocked;
+		} else if (gunName == "rifle") {
+			return rifleUnlocked;
+		} else if (gunName == "shotgun") {
+			return shotgunUnlocked;
+		} else {
+			return false;
+		}
+	}
 
+	public void buttonAction(string gunName){
+		if (gunName == "pistol") {
+			return;
+		} else if (gunName == "rifle") {
+			rifleUnlocked = true;
+		} else if (gunName == "shotgun") {
+			shotgunUnlocked = true;
+		} else {
+			return;
+		}
+		print (pistolUnlocked + " " + rifleUnlocked + " " + shotgunUnlocked);
 	}
 }
