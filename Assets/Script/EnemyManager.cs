bdebbd337bdad9dc1d529aca9a8e1b9ce56adba0
Jacob <jdickins@uoregon.edu>
2016-05-17 23:48:11 -0700

    reset

---
+++ Assets/Script/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public GameObject enemy; 
+    public float spawnTime = 3f;  
+    public Transform[] spawnPoints; 
+
+	public GameManager gameManager;
+
+
+    void Start ()
+    {
+        InvokeRepeating ("Spawn", spawnTime, spawnTime);
+		gameManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager>();
+    }
+
+	void Update()
+	{
+		if (gameManager.winner) {
+			Destroy (this.gameObject);
+		}
+	}
+    void Spawn ()
+    {
+		
+        int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+        Instantiate (enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	public int aliensHaveToKill;
+	public int currentKilledAliens;
+	public bool winner;
+
+	public void KilledAlien()
+	{
+		currentKilledAliens++;
+	}
+
+	void Update()
+	{
+		if (currentKilledAliens >= aliensHaveToKill) 
+		{
+			winner = true;
+		}
+	}
+}
